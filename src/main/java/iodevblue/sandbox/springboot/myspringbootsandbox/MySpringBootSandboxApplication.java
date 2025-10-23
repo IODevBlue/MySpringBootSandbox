50c1e5398bcb883fde20788814da7d8c0ca6740b
Emmanuel E <iodevblue@gmail.com>
2025-10-23 20:29:41 +0100

    Add initial Spring Boot application setup with Maven wrapper and basic structure

---
+++ src/main/java/iodevblue/sandbox/springboot/myspringbootsandbox/MySpringBootSandboxApplication.java
@@ -0,0 +1,13 @@
+package iodevblue.sandbox.springboot.myspringbootsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iodevblue/sandbox/springboot/myspringbootsandbox/MySpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package iodevblue.sandbox.springboot.myspringbootsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
